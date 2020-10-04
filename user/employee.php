28808909c621d59f079326dd949292e2d033baf3
dilshan <dilshansankalpa98@gmail.com>
2020-10-05 00:16:50 +0530

    4

--- user/employee.php
+++ user/employee.php
@@ -14,15 +14,15 @@ if($type == 1){
     $grade = $man["grade"];
 ?>
 employee type : Manager <br>
-<form action="" method="post">
     Desgnation : <br>
-    <input type="text" value="<?php echo $man['designation'] ?>">
+    echo $man['designation'];
     Grade : <br>
-    <select name="grade" id="grade">
-        <option value="1" <?php if($selected == '1'){echo("selected");}?>>junior</option>
-        <option value="2" <?php if($selected == '2'){echo("selected");}?>>senior</option>
-    </select>
-</form>
+    if($man["grade"]==1){
+        echo "junior";
+    }
+    elseif($man["grade"]==2){
+        echo "senior";
+    }
 
 
 <?php
@@ -30,7 +30,7 @@ employee type : Manager <br>
 elseif($type == 2){
 ?>
 Employee type : Kitchen staff <br>
-$ks = mysqli_fetch_assoc($conn->query("select * from kitchenStaff where empId = {$man['empId']}"));
+$ks = mysqli_fetch_assoc($conn->query("select * from kitchenStaff where empId = {$id}"));
 work as a : <?php if($ks["cheffFlag"]==1) echo "Cheff "; if($ks["supportFlag"]==1) echo "Supporter "; if($ks["waiter"]==1) echo "waiter "; ?>
 <br>
 experience level : <br>
@@ -48,32 +48,44 @@ elseif($ks["experience"]==3){
 <?php
 }
 elseif($type == 3){
-    $de = $conn->query("select * from cleaner where empId = {$man['empId']}");
+    $de = $conn->query("select * from cleaner where empId = {$id}");
     $clean = mysqli_fetch_assoc($de);
     ?>
 Employee type : cleaner <br>
 
 working location : <?php echo $clean["location"]; ?>
-
-<?php
-}
-elseif($type == 4){
-?>
-Employee type : receptionist
-
+<br>
 cleaned rooms :
-
 <?php
-$de1 = $conn->query("select * from roomClean where empId = {$man['empId']}");
+$de1 = $conn->query("select * from roomClean where empId = {$id}");
 while($row = mysqli_fetch_assoc($de1)){
 
     echo $row["date"]+" "+ $row["time"]+" "+ $row["roomNo"];
     
 ?>
+
+
 <?php
 }
 ?>
 
 <?php
 }
+elseif($type == 4){
+    $de2 = $conn->query("select * from where recGuest where empId = {$id}");
 ?>
+Employee type : receptionist <br>
+
+handled guests : <br>
+
+<? php
+while($row1 = mysqli_fetch_assoc($de2)){
+    echo $row1["guestId"];
+?>
+<br>
+
+<?php
+}
+}
+?>
+
