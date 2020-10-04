56e34ce093fa049a5a75478496d78c409cf722a0
dilshan <dilshansankalpa98@gmail.com>
2020-10-04 23:51:27 +0530

    second commit

--- login/makenewuser.php
+++ login/makenewuser.php
@@ -1,17 +1,5 @@
 <?php
 require_once "../DB/DB.php";
-$name = $_POST["name"];
-$pw = $_POST["password"];
-$mpw = md5($pw);
-$type = $_POST["type"];
 
-if(empty($conn->query("select type from user where username = {$name} and password = {$mpw};")))
-{
-    $conn->query("insert into user(id,name,password,type) values (NULL,{$name},{$mpw},{$type});");
-    echo "successfully entered a user";
-}
-else{
-    echo "user name is taken";
-}
 
 ?>
