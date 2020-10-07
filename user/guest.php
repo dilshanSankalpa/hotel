8551990bb820d1f911ffceeab96b35fc49a2eb25
dilshan <dilshansankalpa98@gmail.com>
2020-10-07 19:26:14 +0530

    new

--- user/guest.php
+++ user/guest.php
@@ -22,7 +22,7 @@ if($guest["type"]==1){
 }
 elseif($guest["type"]==2){
     echo "guest type : individual<br>";
-$query = $conn->query("SELECT * FROM `individual` WHERE guestId = '{$id}'");
+$query = $conn->query("SELECT * FROM `individual` WHERE guestId = '{$id}';");
     $individual = mysqli_fetch_assoc($query);
     echo "NIC : ".$individual["NIC"]." name ".$individual["name"]." gender : ";
     if($individual["gender"]==1){
@@ -38,17 +38,17 @@ elseif($guest["type"]==3){
     echo "Name of the company : ".$company["name"]." Address ".$company["address"];
 }
 
-$conn->query("create view foodC(foC,amo) as select food.cost,foodGuest.portion from food,foodGuest where  food.foodId = foodGuest.foodId and foodGuest = {$id}");
+$conn->query("create view foodC(foC,amo) as select food.cost,foodGuest.portion from food,foodGuest where food.foodId = foodGuest.foodId and foodGuest.guestId = {$id}");
 $bill = 0;
 $foodCost = 0;
 $roomCost = 0;
 $facilityCost = 0;
-$queryRoomCost = $conn->query("select sum(room.cost) AS c from room,roomGuest where room.No = roomGuest.roomNo and roomGuest.guestId = {$id}");
+$queryRoomCost = $conn->query("select sum(room.cost) AS c from room,roomGuest where room.roomNo = roomGuest.roomNo and roomGuest.guestId = '{$id}';");
 if(isset($queryRoomCost)){
     $roomCostF = mysqli_fetch_assoc($queryRoomCost);
     $roomCost = $roomCostF["c"];
 }
-$queryFacilityCost = $conn->query("select sum(facilityCost) AS c from facility,guestFacility where guestFacility.facilityId = facility.facilityId and guestFacility.guestId = {$id}");
+$queryFacilityCost = $conn->query("select sum(facility.cost) AS c from facility,guestFacility where guestFacility.facilityId = facility.facilityId and guestFacility.guestId = {$id};");
 if(isset($queryFacilityCost)){
     $facilityCostF = mysqli_fetch_assoc($queryFacilityCost);
     $facilityCost = $facilityCostF["c"];
