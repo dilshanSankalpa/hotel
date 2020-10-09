9f0722e5d200ff4a33579b8903c171848bc48472
dilshan <dilshansankalpa98@gmail.com>
2020-10-09 13:41:02 +0530

    new

--- index.php
+++ index.php
@@ -8,7 +8,6 @@
 <body>
     <h1>welcome to hotel management system</h1><br>
     <a href="login/signin.php"><div>Click here to sign in </div></a>
-    <a href="login/signupguest.php"><div>click heree to sign up(Guest only)</div></a>
-    
+   
 </body>
 </html>
