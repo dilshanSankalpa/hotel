68ed5e30abb768595937e2d3032ce5b1dbd18eec
dilshan <dilshansankalpa98@gmail.com>
2020-10-11 22:22:36 +0530

    new

--- user/admin/deletefacility.php
+++ user/admin/deletefacility.php
@@ -2,8 +2,6 @@
 require_once('../../DB/DB.php');
 $id = $_GET["id"];
 $res = $conn->query("delete from facility where faciltyId = {$id}");
-if($res){
-    header("Location:facility.php");
-}
+header("Location:facility.php");
 $conn->close();
 ?>
