c9be35d5e73089d5d7a3eb06e5cd33eb33196cd1
dilshan <dilshansankalpa98@gmail.com>
2020-10-11 22:03:50 +0530

    new

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php require_once('include/header.php'); ?>
-    <h1>welcome to hotel management system</h1><br>
-    <a href="login/signin.php"><div>Click here to sign in </div></a>
+    <h1 class="">welcome to hotel management system</h1><br>
+    <div class="card-body"><a href="login/signin.php" class="card-link">Click here to sign in </a></div>
    
 <?php require_once('include/footer.php');
--- login/signin.php
+++ login/signin.php
@@ -1,11 +1,14 @@
-<?php require_once('../include/header'); ?>
+<?php require_once('../include/header.php'); ?>
 <h2>sign in</h2><br>
-    <form action="checkuser.php" method="post">
+<div>
+<form action="checkuser.php" method="post">
         Enter the user name : <br>
         <input type="text" name="name"><br>
         Enter the pasword <br>
         <input type="password" name="pw"> <br>
         <input type="submit" value="submit">
     </form>
+</div>
+    
     
 <?php require_once('../include/footer.php'); ?>
