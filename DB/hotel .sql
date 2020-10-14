1767a4159d656dc4dc649730e648acd159c85ae4
dilshan <dilshansankalpa98@gmail.com>
2020-10-14 15:55:24 +0530

    finish

--- DB/hotel .sql
+++ DB/hotel .sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Oct 11, 2020 at 11:53 AM
+-- Generation Time: Oct 14, 2020 at 12:24 PM
 -- Server version: 10.4.14-MariaDB
 -- PHP Version: 7.4.10
 
@@ -35,10 +35,10 @@ CREATE TABLE `cheff` (
 -- --------------------------------------------------------
 
 --
--- Table structure for table `cheffGuest`
+-- Table structure for table `cheffFood`
 --
 
-CREATE TABLE `cheffGuest` (
+CREATE TABLE `cheffFood` (
   `empId` int(3) NOT NULL,
   `foodId` int(3) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
@@ -53,6 +53,13 @@ CREATE TABLE `cleaner` (
   `empId` int(4) NOT NULL,
   `location` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `cleaner`
+--
+
+INSERT INTO `cleaner` (`empId`, `location`) VALUES
+(7, 'first floor');
 
 -- --------------------------------------------------------
 
@@ -96,7 +103,10 @@ CREATE TABLE `employee` (
 --
 
 INSERT INTO `employee` (`empId`, `name`, `gender`, `tpNo`, `salaryGrade`, `wsd`, `facilityId`, `id`, `type`) VALUES
-(5, 'wathsala', 1, '01245664', 1, '2020-10-01', NULL, 15, 1);
+(5, 'wathsala', 1, '01245664', 1, '2020-10-01', NULL, 15, 1),
+(6, 'lakmal', 1, '0126547', 1, '2020-10-14', NULL, 16, 4),
+(7, 'hansika', 2, '0124563', 2, '2020-10-07', NULL, 17, 3),
+(8, 'siril', 1, '0124563', 3, '2020-10-07', NULL, 18, 2);
 
 -- --------------------------------------------------------
 
@@ -118,8 +128,8 @@ CREATE TABLE `facility` (
 --
 
 INSERT INTO `facility` (`facilityId`, `location`, `cost`, `rate`, `type`, `name`) VALUES
-(4, 'first floor', 200, 5, 1, 'facility 1'),
-(5, 'garden', 230, 8, 2, 'facillty 2');
+(8, 'blah1', 10, 2, 2, 'admin'),
+(9, 'aff', 120, 10, 2, 'efef');
 
 -- --------------------------------------------------------
 
@@ -290,6 +300,13 @@ CREATE TABLE `kitchenStaff` (
   `waiter` int(1) NOT NULL COMMENT '1 = waiter 0 = not a waiter'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Dumping data for table `kitchenStaff`
+--
+
+INSERT INTO `kitchenStaff` (`empId`, `experience`, `contractPeriod`, `cheffFlag`, `supportFlag`, `waiter`) VALUES
+(8, 1, 1, 1, 0, 0);
+
 -- --------------------------------------------------------
 
 --
@@ -307,6 +324,7 @@ CREATE TABLE `manager` (
 --
 
 INSERT INTO `manager` (`grade`, `designation`, `empId`) VALUES
+(1, 'boss', 5),
 (1, 'boss', 15);
 
 -- --------------------------------------------------------
@@ -344,6 +362,13 @@ CREATE TABLE `recGuest` (
   `time` time NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Dumping data for table `recGuest`
+--
+
+INSERT INTO `recGuest` (`guestId`, `empId`, `date`, `time`) VALUES
+(9, 6, '2020-10-06', '04:17:37');
+
 -- --------------------------------------------------------
 
 --
@@ -378,6 +403,13 @@ CREATE TABLE `roomClean` (
   `date` date NOT NULL,
   `time` time NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `roomClean`
+--
+
+INSERT INTO `roomClean` (`empId`, `roomNo`, `date`, `time`) VALUES
+(7, 1, '2020-10-14', '06:27:14');
 
 -- --------------------------------------------------------
 
@@ -419,7 +451,10 @@ INSERT INTO `user` (`id`, `password`, `type`, `name`) VALUES
 (13, '123', 1, 'individual'),
 (14, '123', 1, 'company'),
 (15, '123', 2, 'manager'),
-(16, '123', 2, 'receptionist');
+(16, '123', 2, 'receptionist'),
+(17, '123', 2, 'cleaner'),
+(18, '123', 2, 'cheff'),
+(19, '123', 3, 'admin');
 
 -- --------------------------------------------------------
 
@@ -443,10 +478,11 @@ ALTER TABLE `cheff`
   ADD PRIMARY KEY (`empId`);
 
 --
--- Indexes for table `cheffGuest`
+-- Indexes for table `cheffFood`
 --
-ALTER TABLE `cheffGuest`
-  ADD PRIMARY KEY (`empId`,`foodId`);
+ALTER TABLE `cheffFood`
+  ADD KEY `empId` (`empId`),
+  ADD KEY `foodId` (`foodId`);
 
 --
 -- Indexes for table `cleaner`
@@ -458,13 +494,16 @@ ALTER TABLE `cleaner`
 -- Indexes for table `company`
 --
 ALTER TABLE `company`
-  ADD PRIMARY KEY (`name`);
+  ADD PRIMARY KEY (`name`),
+  ADD KEY `guestId` (`guestId`);
 
 --
 -- Indexes for table `employee`
 --
 ALTER TABLE `employee`
-  ADD PRIMARY KEY (`empId`);
+  ADD PRIMARY KEY (`empId`),
+  ADD KEY `facilityId` (`facilityId`),
+  ADD KEY `id` (`id`);
 
 --
 -- Indexes for table `facility`
@@ -476,7 +515,8 @@ ALTER TABLE `facility`
 -- Indexes for table `family`
 --
 ALTER TABLE `family`
-  ADD PRIMARY KEY (`hNIC`);
+  ADD PRIMARY KEY (`hNIC`),
+  ADD KEY `guestId` (`guestId`);
 
 --
 -- Indexes for table `food`
@@ -488,7 +528,8 @@ ALTER TABLE `food`
 -- Indexes for table `foodGuest`
 --
 ALTER TABLE `foodGuest`
-  ADD PRIMARY KEY (`guestId`,`foodId`);
+  ADD PRIMARY KEY (`guestId`,`foodId`),
+  ADD KEY `foodId` (`foodId`);
 
 --
 -- Indexes for table `guest`
@@ -500,7 +541,8 @@ ALTER TABLE `guest`
 -- Indexes for table `guestFacility`
 --
 ALTER TABLE `guestFacility`
-  ADD PRIMARY KEY (`facilityId`,`guestId`);
+  ADD PRIMARY KEY (`facilityId`,`guestId`),
+  ADD KEY `guestId` (`guestId`);
 
 --
 -- Indexes for table `guTell`
@@ -512,7 +554,8 @@ ALTER TABLE `guTell`
 -- Indexes for table `individual`
 --
 ALTER TABLE `individual`
-  ADD PRIMARY KEY (`NIC`);
+  ADD PRIMARY KEY (`NIC`),
+  ADD KEY `guestId` (`guestId`);
 
 --
 -- Indexes for table `indoorRReq`
@@ -545,6 +588,13 @@ ALTER TABLE `outdoorSReq`
   ADD PRIMARY KEY (`facilityId`,`sReq`);
 
 --
+-- Indexes for table `recGuest`
+--
+ALTER TABLE `recGuest`
+  ADD PRIMARY KEY (`guestId`,`empId`),
+  ADD KEY `empId` (`empId`);
+
+--
 -- Indexes for table `room`
 --
 ALTER TABLE `room`
@@ -554,13 +604,15 @@ ALTER TABLE `room`
 -- Indexes for table `roomClean`
 --
 ALTER TABLE `roomClean`
-  ADD PRIMARY KEY (`empId`,`roomNo`);
+  ADD PRIMARY KEY (`empId`,`roomNo`),
+  ADD KEY `roomNo` (`roomNo`);
 
 --
 -- Indexes for table `roomGuest`
 --
 ALTER TABLE `roomGuest`
-  ADD PRIMARY KEY (`guestId`,`roomNo`);
+  ADD PRIMARY KEY (`guestId`,`roomNo`),
+  ADD KEY `roomNo` (`roomNo`);
 
 --
 -- Indexes for table `user`
@@ -573,7 +625,7 @@ ALTER TABLE `user`
 -- Indexes for table `waiter`
 --
 ALTER TABLE `waiter`
-  ADD PRIMARY KEY (`empId`);
+  ADD PRIMARY KEY (`empId`,`sStyle`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -583,13 +635,13 @@ ALTER TABLE `waiter`
 -- AUTO_INCREMENT for table `employee`
 --
 ALTER TABLE `employee`
-  MODIFY `empId` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `empId` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT for table `facility`
 --
 ALTER TABLE `facility`
-  MODIFY `facilityId` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `facilityId` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT for table `food`
@@ -613,11 +665,92 @@ ALTER TABLE `room`
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int(4) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+  MODIFY `id` int(4) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
 
 --
 -- Constraints for dumped tables
 --
+
+--
+-- Constraints for table `cheff`
+--
+ALTER TABLE `cheff`
+  ADD CONSTRAINT `cheff_ibfk_1` FOREIGN KEY (`empId`) REFERENCES `employee` (`empId`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `cheffFood`
+--
+ALTER TABLE `cheffFood`
+  ADD CONSTRAINT `cheffFood_ibfk_1` FOREIGN KEY (`empId`) REFERENCES `employee` (`empId`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `cheffFood_ibfk_2` FOREIGN KEY (`foodId`) REFERENCES `food` (`foodId`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `cleaner`
+--
+ALTER TABLE `cleaner`
+  ADD CONSTRAINT `cleaner_ibfk_1` FOREIGN KEY (`empId`) REFERENCES `employee` (`empId`);
+
+--
+-- Constraints for table `company`
+--
+ALTER TABLE `company`
+  ADD CONSTRAINT `company_ibfk_1` FOREIGN KEY (`guestId`) REFERENCES `guest` (`guestId`);
+
+--
+-- Constraints for table `employee`
+--
+ALTER TABLE `employee`
+  ADD CONSTRAINT `employee_ibfk_1` FOREIGN KEY (`facilityId`) REFERENCES `facility` (`facilityId`) ON DELETE SET NULL ON UPDATE CASCADE,
+  ADD CONSTRAINT `employee_ibfk_2` FOREIGN KEY (`id`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `family`
+--
+ALTER TABLE `family`
+  ADD CONSTRAINT `family_ibfk_1` FOREIGN KEY (`guestId`) REFERENCES `guest` (`guestId`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `foodGuest`
+--
+ALTER TABLE `foodGuest`
+  ADD CONSTRAINT `foodGuest_ibfk_1` FOREIGN KEY (`foodId`) REFERENCES `food` (`foodId`) ON UPDATE CASCADE;
+
+--
+-- Constraints for table `guestFacility`
+--
+ALTER TABLE `guestFacility`
+  ADD CONSTRAINT `guestFacility_ibfk_1` FOREIGN KEY (`facilityId`) REFERENCES `facility` (`facilityId`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `guestFacility_ibfk_2` FOREIGN KEY (`guestId`) REFERENCES `guest` (`guestId`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `guTell`
+--
+ALTER TABLE `guTell`
+  ADD CONSTRAINT `guTell_ibfk_1` FOREIGN KEY (`guestId`) REFERENCES `guest` (`guestId`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `individual`
+--
+ALTER TABLE `individual`
+  ADD CONSTRAINT `individual_ibfk_1` FOREIGN KEY (`guestId`) REFERENCES `guest` (`guestId`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `indoorRReq`
+--
+ALTER TABLE `indoorRReq`
+  ADD CONSTRAINT `indoorRReq_ibfk_1` FOREIGN KEY (`facilityId`) REFERENCES `facility` (`facilityId`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `kitchenStaff`
+--
+ALTER TABLE `kitchenStaff`
+  ADD CONSTRAINT `kitchenStaff_ibfk_1` FOREIGN KEY (`empId`) REFERENCES `employee` (`empId`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `manager`
+--
+ALTER TABLE `manager`
+  ADD CONSTRAINT `manager_ibfk_1` FOREIGN KEY (`empId`) REFERENCES `employee` (`empId`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Constraints for table `outdoorGReq`
@@ -630,6 +763,31 @@ ALTER TABLE `outdoorGReq`
 --
 ALTER TABLE `outdoorSReq`
   ADD CONSTRAINT `outdoorSReq_ibfk_1` FOREIGN KEY (`facilityId`) REFERENCES `facility` (`facilityId`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `recGuest`
+--
+ALTER TABLE `recGuest`
+  ADD CONSTRAINT `recGuest_ibfk_1` FOREIGN KEY (`empId`) REFERENCES `employee` (`empId`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `roomClean`
+--
+ALTER TABLE `roomClean`
+  ADD CONSTRAINT `roomClean_ibfk_1` FOREIGN KEY (`roomNo`) REFERENCES `room` (`roomNo`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `roomGuest`
+--
+ALTER TABLE `roomGuest`
+  ADD CONSTRAINT `roomGuest_ibfk_1` FOREIGN KEY (`guestId`) REFERENCES `guest` (`guestId`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `roomGuest_ibfk_2` FOREIGN KEY (`roomNo`) REFERENCES `room` (`roomNo`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `waiter`
+--
+ALTER TABLE `waiter`
+  ADD CONSTRAINT `waiter_ibfk_1` FOREIGN KEY (`empId`) REFERENCES `employee` (`empId`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
